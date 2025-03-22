1a3723a21c77c1144e6de5320bdcb1a4d2364d78
Brent Koogler <67485948+jozbee@users.noreply.github.com>
2025-03-22 15:30:54 -0500

    reformatting

--- exp_mpc/stewart_min/spec.py
+++ exp_mpc/stewart_min/spec.py
@@ -9,36 +9,55 @@ n = 40
 leg_min = 1.2
 leg_max = 1.8
 leg_mid = 1.5
-dt = 1. / 250.
+dt = 1.0 / 250.0
 
 center = np.array([-2.53480164e-05, -1.86958364e-04, -9.81776321e-02])
 center = center.reshape(1, 3)  # for broadcasting with tops and bots
-bots = np.array([
-    [ 0.090902, -0.952057, -1.4     ],
-    [ 0.870168,  0.397276, -1.400285],
-    [ 0.779349,  0.554337, -1.4     ],
-    [-0.779347,  0.553952, -1.400037],
-    [-0.86997 ,  0.397276, -1.4     ],
-    [-0.090113, -0.952578, -1.400363],
-])
-tops = np.array([
-    [ 0.32637129, -0.32151598, -0.09877087],
-    [ 0.44215802, -0.118274  , -0.09750311],
-    [ 0.1149581 ,  0.44309603, -0.09906373],
-    [-0.11874097,  0.4420145 , -0.09723438],
-    [-0.44143789, -0.12203837, -0.09888594],
-    [-0.32346064, -0.32440392, -0.09760777],
-])
+bots = np.array(
+    [
+        [0.090902, -0.952057, -1.4],
+        [0.870168, 0.397276, -1.400285],
+        [0.779349, 0.554337, -1.4],
+        [-0.779347, 0.553952, -1.400037],
+        [-0.86997, 0.397276, -1.4],
+        [-0.090113, -0.952578, -1.400363],
+    ]
+)
+tops = np.array(
+    [
+        [0.32637129, -0.32151598, -0.09877087],
+        [0.44215802, -0.118274, -0.09750311],
+        [0.1149581, 0.44309603, -0.09906373],
+        [-0.11874097, 0.4420145, -0.09723438],
+        [-0.44143789, -0.12203837, -0.09888594],
+        [-0.32346064, -0.32440392, -0.09760777],
+    ]
+)
 
 
 center_bots = bots - center
 center_tops = tops - center
-state0 = np.array([
-    center[0, 0], center[0, 1], center[0, 2],
-    0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0,
-    0.0, 0.0, 0.0,
-])
+state0 = np.array(
+    [
+        center[0, 0],
+        center[0, 1],
+        center[0, 2],
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+    ]
+)
+
+
+def _ca_sx(name: str) -> ca.SX:
+    """Create a casadi variable without errors"""
+    return ca.SX.sym(name)  # type: ignore
 
 
 def gen_stewart_model():
@@ -47,44 +66,69 @@ def gen_stewart_model():
 
     # TODO(jozbee): make cleaner looking
 
-    x = ca.SX.sym("x")
-    y = ca.SX.sym("y")
-    z = ca.SX.sym("z")
-    phi = ca.SX.sym("phi")
-    theta = ca.SX.sym("theta")
-    psi = ca.SX.sym("psi")
-    x_dot = ca.SX.sym("x_dot")
-    y_dot = ca.SX.sym("y_dot")
-    z_dot = ca.SX.sym("z_dot")
-    phi_dot = ca.SX.sym("phi_dot")
-    theta_dot = ca.SX.sym("theta_dot")
-    psi_dot = ca.SX.sym("psi_dot")
-    model.x = ca.vertcat(x, y, z, phi, theta, psi, x_dot, y_dot, z_dot, phi_dot, theta_dot, psi_dot)
+    x = _ca_sx("x")
+    y = _ca_sx("y")
+    z = _ca_sx("z")
+    phi = _ca_sx("phi")
+    theta = _ca_sx("theta")
+    psi = _ca_sx("psi")
+    x_dot = _ca_sx("x_dot")
+    y_dot = _ca_sx("y_dot")
+    z_dot = _ca_sx("z_dot")
+    phi_dot = _ca_sx("phi_dot")
+    theta_dot = _ca_sx("theta_dot")
+    psi_dot = _ca_sx("psi_dot")
+    model.x = ca.vertcat(  # type: ignore
+        x,
+        y,
+        z,
+        phi,
+        theta,
+        psi,
+        x_dot,
+        y_dot,
+        z_dot,
+        phi_dot,
+        theta_dot,
+        psi_dot,
+    )
 
-    x_p = ca.SX.sym("x_p")
-    y_p = ca.SX.sym("y_p")
-    z_p = ca.SX.sym("z_p")
-    phi_p = ca.SX.sym("phi_p")
-    theta_p = ca.SX.sym("theta_p")
-    psi_p = ca.SX.sym("psi_p")
-    x_dot_p = ca.SX.sym("x_dot_p")
-    y_dot_p = ca.SX.sym("y_dot_p")
-    z_dot_p = ca.SX.sym("z_dot_p")
-    phi_dot_p = ca.SX.sym("phi_dot_p")
-    theta_dot_p = ca.SX.sym("theta_dot_p")
-    psi_dot_p = ca.SX.sym("psi_dot_p")
-    model.xdot = ca.vertcat(x_p, y_p, z_p, phi_p, theta_p, psi_p, x_dot_p, y_dot_p, z_dot_p, phi_dot_p, theta_dot_p, psi_dot_p)
+    x_p = _ca_sx("x_p")
+    y_p = _ca_sx("y_p")
+    z_p = _ca_sx("z_p")
+    phi_p = _ca_sx("phi_p")
+    theta_p = _ca_sx("theta_p")
+    psi_p = _ca_sx("psi_p")
+    x_dot_p = _ca_sx("x_dot_p")
+    y_dot_p = _ca_sx("y_dot_p")
+    z_dot_p = _ca_sx("z_dot_p")
+    phi_dot_p = _ca_sx("phi_dot_p")
+    theta_dot_p = _ca_sx("theta_dot_p")
+    psi_dot_p = _ca_sx("psi_dot_p")
+    model.xdot = ca.vertcat(
+        x_p,
+        y_p,
+        z_p,
+        phi_p,
+        theta_p,
+        psi_p,
+        x_dot_p,
+        y_dot_p,
+        z_dot_p,
+        phi_dot_p,
+        theta_dot_p,
+        psi_dot_p,
+    )
 
-    x_u = ca.SX.sym("x_u")
-    y_u = ca.SX.sym("y_u")
-    z_u = ca.SX.sym("z_u")
-    phi_u = ca.SX.sym("phi_u")
-    theta_u = ca.SX.sym("theta_u")
-    psi_u = ca.SX.sym("psi_u")
+    x_u = _ca_sx("x_u")
+    y_u = _ca_sx("y_u")
+    z_u = _ca_sx("z_u")
+    phi_u = _ca_sx("phi_u")
+    theta_u = _ca_sx("theta_u")
+    psi_u = _ca_sx("psi_u")
     model.u = ca.vertcat(x_u, y_u, z_u, phi_u, theta_u, psi_u)
 
-    
-    model.f_expl_expr = ca.vertcat(
+    model.f_expl_expr = ca.vertcat(  # type: ignore
         x_dot + 0.5 * dt * x_u,
         y_dot + 0.5 * dt * y_u,
         z_dot + 0.5 * dt * z_u,
@@ -96,7 +140,7 @@ def gen_stewart_model():
         z_u,
         phi_u,
         theta_u,
-        psi_u
+        psi_u,
     )
 
     return model
@@ -107,13 +151,17 @@ def get_R(phi: ca.SX, theta: ca.SX, psi: ca.SX) -> ca.SX:
 
     # first row
     R[0, 0] = ca.cos(psi) * ca.cos(theta)
-    R[0, 1] = ca.sin(phi) * ca.sin(theta) * ca.cos(psi) - ca.sin(psi) * ca.cos(phi)
-    R[0, 2] = ca.sin(phi) * ca.sin(psi) + ca.sin(theta) * ca.cos(phi) * ca.cos(psi)
+    R[0, 1] = ca.sin(phi) * ca.sin(theta) * ca.cos(psi)
+    R[0, 1] -= ca.sin(psi) * ca.cos(phi)
+    R[0, 2] = ca.sin(phi) * ca.sin(psi)
+    R[0, 2] += ca.sin(theta) * ca.cos(phi) * ca.cos(psi)
 
     # second row
     R[1, 0] = ca.sin(psi) * ca.cos(theta)
-    R[1, 1] = ca.sin(phi) * ca.sin(psi) * ca.sin(theta) + ca.cos(phi) * ca.cos(psi)
-    R[1, 2] = -ca.sin(phi) * ca.cos(psi) + ca.sin(psi) * ca.sin(theta) * ca.cos(phi)
+    R[1, 1] = ca.sin(phi) * ca.sin(psi) * ca.sin(theta)
+    R[1, 1] += ca.cos(phi) * ca.cos(psi)
+    R[1, 2] = -ca.sin(phi) * ca.cos(psi)
+    R[1, 2] += ca.sin(psi) * ca.sin(theta) * ca.cos(phi)
 
     # third row
     R[2, 0] = -ca.sin(theta)
@@ -134,12 +182,12 @@ def get_PHI(phi: ca.SX, theta: ca.SX, psi: ca.SX) -> ca.SX:
     # second row
     PHI[1, 0] = 0
     PHI[1, 1] = ca.cos(phi)
-    PHI[1, 2] = ca.sin(phi)*ca.cos(theta)
+    PHI[1, 2] = ca.sin(phi) * ca.cos(theta)
 
     # third row
     PHI[2, 0] = 0
     PHI[2, 1] = -ca.sin(phi)
-    PHI[2, 2] = ca.cos(phi)*ca.cos(theta)
+    PHI[2, 2] = ca.cos(phi) * ca.cos(theta)
 
     return PHI
 
@@ -181,7 +229,7 @@ def get_angular_velocity(model: at.AcadosModel) -> ca.SX:
     return PHI @ euler_dot
 
 
-def get_squared_lengths(model: at.AcadosModel) -> ca.SX:
+def get_squared_lengths(model: at.AcadosModel) -> ca.DM:
     state = model.x
 
     x = state[0]
@@ -203,7 +251,7 @@ def get_squared_lengths(model: at.AcadosModel) -> ca.SX:
     for diff in diffs:
         squared_length = diff.T @ diff  # compute squared Euclidean norm
         squared_lengths.append(squared_length)
-        
+
     return ca.vertcat(*squared_lengths)
 
 
@@ -249,12 +297,12 @@ def gen_stewart_ocp(model):
     ocp.constraints.x0 = state0
 
     # options
-    ocp.solver_options.sq_solver = "PARTIAL_CONDENSING_HPIPM"
+    ocp.solver_options.qp_solver = "PARTIAL_CONDENSING_HPIPM"
     ocp.solver_options.hessian_approx = "GAUSS_NEWTON"
     ocp.solver_options.integrator_type = "ERK"
     ocp.solver_options.nlp_solver_type = "SQP"
     ocp.solver_options.qp_solver_iter_max = 4
-    ocp.qp_solver_cond_N = n  # no condensing, for now
+    ocp.solver_options.qp_solver_cond_N = n  # no condensing, for now
 
     # horizon
     ocp.solver_options.N_horizon = n
@@ -299,6 +347,7 @@ class PoseDot2:
 @dataclasses.dataclass
 class TableStats:
     """Statistics of the solution to the Stewart platform OCP."""
+
     time: float
     status: int
     cost: float
@@ -307,6 +356,7 @@ class TableStats:
 @dataclasses.dataclass
 class TableSol:
     """A solution to the Stewart platform OCP."""
+
     x: np.ndarray
     u: np.ndarray
     stats: TableStats
@@ -316,7 +366,7 @@ class TableSol:
 
     def pose_dot_at(self, i: int) -> PoseDot:
         return PoseDot(*self.x[i, 6:12])
-    
+
     def pose_dot2_at(self, i: int) -> PoseDot2:
         return PoseDot2(*self.u[i, :6])
 
@@ -324,23 +374,34 @@ class TableSol:
 @dataclasses.dataclass
 class TableMPC:
     """A model predictive controller for the Stewart platform."""
+
     model: at.AcadosModel
     ocp: at.AcadosOcp
     solver: at.AcadosOcpSolver
 
     x0: np.ndarray = dataclasses.field(default_factory=lambda: state0)
-    leg_ref: np.ndarray = dataclasses.field(default_factory=lambda: np.ones(6) * leg_mid**2)
+    leg_ref: np.ndarray = dataclasses.field(
+        default_factory=lambda: np.ones(6) * leg_mid**2
+    )
     a_ref: np.ndarray = dataclasses.field(default_factory=lambda: np.zeros(3))
-    omega_ref: np.ndarray = dataclasses.field(default_factory=lambda: np.zeros(3))
-    zero_control: np.ndarray = dataclasses.field(default_factory=lambda: np.zeros(6))
+    omega_ref: np.ndarray = dataclasses.field(
+        default_factory=lambda: np.zeros(3)
+    )
+    zero_control: np.ndarray = dataclasses.field(
+        default_factory=lambda: np.zeros(6)
+    )
 
-    state_sol: np.ndarray = dataclasses.field(default_factory=lambda: np.zeros((n + 1, 12)))
-    control_sol: np.ndarray = dataclasses.field(default_factory=lambda: np.zeros((n, 6)))
+    state_sol: np.ndarray = dataclasses.field(
+        default_factory=lambda: np.zeros((n + 1, 12))
+    )
+    control_sol: np.ndarray = dataclasses.field(
+        default_factory=lambda: np.zeros((n, 6))
+    )
 
     solution_status: int = 0
     solve_time: float = 0.0
     cost: float = 0.0
-    
+
     @classmethod
     def create_default(cls) -> TableMPC:
         model = gen_stewart_model()
@@ -352,12 +413,16 @@ class TableMPC:
         return mpc
 
     def set_weights(self, w_a=1.0, w_omega=1.0, w_leg=1.0, w_control=1.0):
-        W = np.diag(np.concat([
-            np.ones(3) * w_a,
-            np.ones(3) * w_omega,
-            np.ones(6) * w_leg,
-            np.ones(6) * w_control
-        ]))
+        W = np.diag(
+            np.concat(
+                [
+                    np.ones(3) * w_a,
+                    np.ones(3) * w_omega,
+                    np.ones(6) * w_leg,
+                    np.ones(6) * w_control,
+                ]
+            )
+        )
         W_e = np.diag(np.ones(6) * w_leg)
         for i in range(n):
             self.solver.cost_set(i, "W", W)
@@ -370,20 +435,19 @@ class TableMPC:
             self.omega_ref = omega_ref
 
         for i in range(n):
-            yref = np.concatenate([
-                self.a_ref, self.omega_ref, self.leg_ref, self.zero_control])
+            yref = np.concatenate(
+                [self.a_ref, self.omega_ref, self.leg_ref, self.zero_control]
+            )
             self.solver.cost_set(i, "yref", yref)
 
-        yref_e = np.concatenate([
-            self.leg_ref])
+        yref_e = np.concatenate([self.leg_ref])
         self.solver.cost_set(n, "yref", yref_e)
 
     def solve(self, x0):
         assert x0.shape == (12,)
         self.x0 = x0
 
-        yref_e = np.concatenate([
-            self.leg_ref])
+        yref_e = np.concatenate([self.leg_ref])
         self.solver.cost_set(n, "yref", yref_e)
 
         self.solver.constraints_set(0, "lbx", self.x0)
@@ -392,7 +456,7 @@ class TableMPC:
         self.solver.solve()
 
         self.solution_status = self.solver.get_status()
-        self.solve_time = self.solver.get_stats("time_tot")
+        self.solve_time = float(self.solver.get_stats("time_tot"))
         self.cost = self.solver.get_cost()
 
         for i in range(n):
